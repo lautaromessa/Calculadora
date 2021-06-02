3458537c91ad2e1f188f278e5cad4d7836bab609
lautaromessa <tutimessa@hotmail.com>
2021-06-02 16:07:07 -0300

    Subiendo Calculadora

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+const sumar = require ("./sumar.js");
+const restar = require ("./restar.js");
+const multiplicar = require ("./multiplicar.js");
+const dividir = require ("./dividir.js");
+
+console.log (sumar(4,9))
+console.log (restar(10,8))
+console.log (multiplicar(3,5))
+console.log (multiplicar(0,7))
+console.log (dividir(8,4))
+console.log (dividir(8,0))
+
